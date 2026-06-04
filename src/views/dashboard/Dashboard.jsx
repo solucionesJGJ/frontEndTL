f9356b8a86b48a4c658163840108ce99e2891cfd
Jose Vinet <jvinetc@gmail.com>
2026-06-04 15:34:37 -0400

    correcciones de ambiente

--- src/views/dashboard/Dashboard.jsx
+++ src/views/dashboard/Dashboard.jsx
@@ -43,12 +43,12 @@ import {
   cilUserFemale,
 } from '@coreui/icons'
 
-import avatar1 from 'src/assets/images/avatars/1.jpg'
+/* import avatar1 from 'src/assets/images/avatars/1.jpg'
 import avatar2 from 'src/assets/images/avatars/2.jpg'
 import avatar3 from 'src/assets/images/avatars/3.jpg'
 import avatar4 from 'src/assets/images/avatars/4.jpg'
 import avatar5 from 'src/assets/images/avatars/5.jpg'
-import avatar6 from 'src/assets/images/avatars/6.jpg'
+import avatar6 from 'src/assets/images/avatars/6.jpg' */
 
 import WidgetsBrand from '../widgets/WidgetsBrand'
 import WidgetsDropdown from '../widgets/WidgetsDropdown'
@@ -87,7 +87,7 @@ const Dashboard = () => {
 
   const tableExample = [
     {
-      avatar: { src: avatar1, status: 'success' },
+      avatar: { src: '', status: 'success' },
       user: {
         name: 'Yiorgos Avraamu',
         new: true,
@@ -103,7 +103,7 @@ const Dashboard = () => {
       activity: '10 sec ago',
     },
     {
-      avatar: { src: avatar2, status: 'danger' },
+      avatar: { src: '', status: 'danger' },
       user: {
         name: 'Avram Tarasios',
         new: false,
@@ -119,7 +119,7 @@ const Dashboard = () => {
       activity: '5 minutes ago',
     },
     {
-      avatar: { src: avatar3, status: 'warning' },
+      avatar: { src: '', status: 'warning' },
       user: { name: 'Quintin Ed', new: true, registered: 'Jan 1, 2023' },
       country: { name: 'India', flag: cifIn },
       usage: {
@@ -131,7 +131,7 @@ const Dashboard = () => {
       activity: '1 hour ago',
     },
     {
-      avatar: { src: avatar4, status: 'secondary' },
+      avatar: { src: '' , status: 'secondary' },
       user: { name: 'Enéas Kwadwo', new: true, registered: 'Jan 1, 2023' },
       country: { name: 'France', flag: cifFr },
       usage: {
@@ -143,7 +143,7 @@ const Dashboard = () => {
       activity: 'Last month',
     },
     {
-      avatar: { src: avatar5, status: 'success' },
+      avatar: { src: '', status: 'success' },
       user: {
         name: 'Agapetus Tadeáš',
         new: true,
@@ -159,7 +159,7 @@ const Dashboard = () => {
       activity: 'Last week',
     },
     {
-      avatar: { src: avatar6, status: 'danger' },
+      avatar: { src: '', status: 'danger' },
       user: {
         name: 'Friderik Dávid',
         new: true,
